ed591aad581fc9eede40dfc37129c204f7164f81
Denchouka <denchouka@163.com>
2024-09-16 15:20:33 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+# 使用官方的Java运行时环境作为基础镜像
+FROM openjdk:8-jdk-alpine
+
+MAINTAINER Denchouka
+
+# 设置工作目录
+WORKDIR /app
+
+# 将项目的jar文件复制到容器中
+COPY target/hello-world-1.1.0.jar /app/hello-world-1.1.0.jar.jar
+
+# 暴露应用程序使用的端口
+EXPOSE 8889
+
+# 运行应用程序
+ENTRYPOINT ["java", "-jar", "/app/hello-world-1.1.0.jar.jar"]
+
---
+++ src/main/java/tch/cool/demos/web/BasicController.java
@@ -0,0 +1,14 @@
+
+package tch.cool.demos.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
